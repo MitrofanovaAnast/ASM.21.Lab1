6ef1e0c203efe01c05b4c562d35dbffc25353774
OxiSgen <60621483+OxiSgen@users.noreply.github.com>
2021-09-22 17:51:21 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -2,7 +2,7 @@ import asm2104.st00.main
 import asm2105.st00.main
 import aam2107.st00.main
 #import aam2107.st14.main
-import aam2107.st04.main
+import aam2107.st03.main
 #	добавить импорт своего модуля по шаблону
 #	import asm<код группы>.st<номер по журналу>.main
 
@@ -11,7 +11,7 @@ MENU = [
 		["[2105-00] Образец 2105", asm2105.st00.main.main],
 		["[2107-00] Образец 2107", aam2107.st00.main.main],
 		#["[2107-00] Sudakov", aam2107.st14.main.main],
-		["[2107-00] Gladkov", aam2107.st04.main.main]
+		["[2107-00] Gladkov", aam2107.st03.main.main]
 #		добавить пункт меню для вызова своей главной функции по шаблону:
 #		["[<код группы>-<номер по журналу>] <Фамилия>", <ссылка на функцию>],
 ]
