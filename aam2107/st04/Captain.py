6d755a34d8d7a2222552051eb64ca296b4457429
oxisgen <oxisgen049@mail.ru>
2021-09-22 13:22:33 +0300

    -

---
+++ aam2107/st04/Captain.py
@@ -0,0 +1,11 @@
+from Stusent import Student
+
+
+class Captain(Student):
+    def __init__(self, name='', surname='', age='', grants=''):
+        super().__init__(name, surname, age)
+        self.__grants = grants
+
+    def __str__(self):
+        return super().__str__() + f', Надбавка { self.__grants }'
+
---
+++ aam2107/st04/main.py
@@ -0,0 +1,27 @@
+from kartoteka import MenuCartoteka
+
+cart = MenuCartoteka()
+container = {
+    1: ("Добавить элемент", cart.add),
+    2: ("Вывести элемент на экран", cart.print),
+    3: ("Выгрузить в файл", cart.file_read),
+    4: ("Загрузить из файла", cart.file_write),
+    5: ("Очистить", cart.clear),
+    6: ("Редактировать", cart.change),
+}
+
+
+def main():
+    print("------------------------------")
+    for i, item in container.items():
+        print(i, ": ", item[0])
+    print("------------------------------")
+    return int(input())
+
+
+if __name__ == '__main__':
+    try:
+        while True:
+            container[main()][1]()
+    except Exception as ex:
+        print(ex, "\nbye")
