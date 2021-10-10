c959d445545fd06e79cdf7e66b3b687e7093f005
devindn <devin.aa1705@asugubkin.ru>
2021-10-10 16:43:10 +0300

    Devin

--- asm2104/st04/fileIO.py
+++ asm2104/st04/fileIO.py
@@ -3,9 +3,9 @@ import pickle
 
 class FileIO:
     def output(self,newList):
-        with open('file.pickle', 'wb') as f:
+        with open('asm2104/st04/file.pickle', 'wb') as f:
             pickle.dump(newList, f)
 
     def input(self,newList):
-        with open('file.pickle', 'rb') as f:
+        with open('asm2104/st04/file.pickle', 'rb') as f:
             return newList+pickle.load(f)
