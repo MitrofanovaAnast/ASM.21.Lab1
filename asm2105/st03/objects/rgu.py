34c05498077a540c95c3bb1ee349ccd88ff4851d
Liana Bogdanova <liana9bogdanova@gmail.com>
2021-10-10 19:27:56 +0300

    Bofdanova

---
+++ asm2105/st03/objects/rgu.py
@@ -0,0 +1,5 @@
+
+class rgu:
+    def __init__(self):
+        self.name='РГУ нефти и газа'
+        self.members={}
---
+++ asm2105/st03/objects/worker.py
@@ -0,0 +1,11 @@
+from .item import item
+
+class worker(item):
+    experience=int()
+    def __init__(self, strategy):
+        item.__init__(self, strategy)
+
+    def __str__(self):
+        text=super().__str__()
+        text+=f'Стаж: {self.experience}\n'
+
