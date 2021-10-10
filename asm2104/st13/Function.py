9db88fd4604761671590387e3b9a6776999418de
Axiom <90249144+Migranov-Artur@users.noreply.github.com>
2021-10-10 21:36:34 +0500

    Add files via upload

---
+++ asm2104/st13/Function.py
@@ -0,0 +1,23 @@
+from Adaptation import Adaptation
+from Original import Original
+import pickle
+    
+class Simple():
+    def print(self, List):
+        for i in List:
+            print(i.strSimple())
+            
+class Editing():
+    def print(self, List):
+        for i in List:
+            print(i.strEditing())
+            
+class PickleDump():
+    def print(self, List):
+        with open('data_pickle.io','wb') as f:
+            pickle.dump(List,f)
+            
+class PickleLoad():
+    def print(self, List):
+        with open('data_pickle.io','rb') as f:
+            return pickle.load(f)
---
+++ asm2104/st13/Original.py
@@ -0,0 +1,19 @@
+from Adaptation import Adaptation
+
+class Original(Adaptation):
+    def __init__(self, name='', episodenum=24, genre=''):
+        self.name=name
+        self.genre=genre
+        self.episodenum=episodenum
+
+        
+    def strSimple(self):
+        return "Сериал:\n"+"Название:{} Жанр:{} Количество серий:{} ".format(self.name,self.genre,self.episodenum,)
+        
+    def strEditing(self):
+        return "Сериал:\n"+"Название:{} Жанр:{} Количество серий:{} ".format(self.name,self.genre,self.episodenum,)
+        
+    def input(self):
+        self.name=input("Название:")
+        self.genre=input("Жанр:")
+        self.episodenum=int(input("Количество серий:"))
