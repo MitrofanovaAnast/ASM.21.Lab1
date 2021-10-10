bc7f62352f020d3d5aa14da3ed073b5a7b1ae15d
NikitaLutskovich <36255798+NikitaLutskovich@users.noreply.github.com>
2021-10-11 08:20:58 +1100

    Lutskovich

--- asm2104/st11/FileIO.py
+++ asm2104/st11/FileIO.py
@@ -4,10 +4,10 @@ import pickle
 class FileIO:
     @staticmethod
     def output(e_list):
-        with open('output.pickle', 'wb') as f:
+        with open('asm2104/st11/output.pickle', 'wb') as f:
             pickle.dump(e_list, f)
 
     @staticmethod
     def input(lenght, e_list):
-        with open('output.pickle', 'rb') as f:
+        with open('asm2104/st11/output.pickle', 'rb') as f:
             return pickle.load(f)
