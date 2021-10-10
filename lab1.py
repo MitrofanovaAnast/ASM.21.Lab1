acbd7af510aee39e39c47d4892589dd735873a0f
Korotkova Sofiya <sofiya78995@gmail.com>
2021-10-10 21:23:24 +0300

    Korotkova

--- lab1.py
+++ lab1.py
@@ -16,6 +16,7 @@ import asm2104.st12.main
 import asm2105.st14.main
 import asm2104.st15.main
 import asm2104.st16.main
+import asm2104.st08.main
 # import asm2104.st13.main
 #	добавить импорт своего модуля по шаблону
 #	import asm<код группы>.st<номер по журналу>.main
@@ -39,7 +40,8 @@ MENU = [
 		["[2104-12] Макарова", asm2104.st12.main.main],
 		["[2105-14] Самушкова", asm2105.st14.main.main],
 		["[2104-14] Polyakova", asm2104.st15.main.main],
-		['[2104-16] Saichkina', asm2104.st16.main.main]
+		['[2104-16] Saichkina', asm2104.st16.main.main],
+		["[2104-08] Korotkova", asm2104.st08.main.main]
 		# ["[2104-13] Migranov", asm2104.st13.main.main],
 ]
 
