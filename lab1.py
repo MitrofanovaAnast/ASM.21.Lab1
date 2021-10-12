5e8bcc33f81127b1a97601060d429b8d82d122e2
dl <dl@bugtraq.ru>
2021-10-12 20:43:44 +0300

    (*)

--- lab1.py
+++ lab1.py
@@ -1,7 +1,7 @@
 import asm2104.st00.main
 import asm2105.st00.main
 import aam2107.st00.main
-#import aam2107.st14.main
+#import aam2107.st13.main
 import aam2107.st03.main
 import asm2105.st04.main
 import asm2104.st12.main
@@ -32,21 +32,21 @@ MENU = [
 		["[2107-00] Образец 2107", aam2107.st00.main.main],
 #		добавить пункт меню для вызова своей главной функции по шаблону:
 #		["[<код группы>-<номер по журналу>] <Фамилия>", <ссылка на функцию>],
-		#["[2107-00] Sudakov", aam2107.st14.main.main],
-		["[2107-00] Gladkov", aam2107.st03.main.main],
+#		["[2107-13] Sudakov", aam2107.st13.main.main],
+		["[2107-03] Gladkov", aam2107.st03.main.main],
 		["[2105-04] Dautov", asm2105.st04.main.main],
 		["[2104-06] Kim", asm2104.st06.main.main],
 		["[2104-11] Lutskovich", asm2104.st11.main.main],
 		["[2104-14] Pezhemsky", asm2104.st14.main.main],
 		["[2104-04] Devin", asm2104.st04.main.main],
-		["[2105-20 Kharisov", asm2105.st20.main.main],
+		["[2105-20] Kharisov", asm2105.st20.main.main],
 		["[2107-04] Zhilina", aam2107.st04.main.main],
 		["[2105-15] Semitko", asm2105.st15.main.main],
 		["[2105-02] Astafeva", asm2105.st02.main.main],
 		["[2105-03] БОГДАНОВА", asm2105.st03.main.main],
 		["[2104-12] Макарова", asm2104.st12.main.main],
 		["[2105-14] Самушкова", asm2105.st14.main.main],
-		["[2104-14] Polyakova", asm2104.st15.main.main],
+		["[2104-15] Polyakova", asm2104.st15.main.main],
 		['[2104-16] Saichkina', asm2104.st16.main.main],
 		["[2104-08] Korotkova", asm2104.st08.main.main],
 		["[2105-18] Tukhvatullina", asm2105.st18.main.main],
