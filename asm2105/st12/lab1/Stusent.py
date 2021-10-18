4cf8ba082eba026a7efab033bc36a88a44ac99fc
Pan4ick <63975286+Pan4ick@users.noreply.github.com>
2021-10-18 20:46:41 +0300

    Panov

---
+++ asm2105/st12/lab1/Stusent.py
@@ -0,0 +1,34 @@
+if __name__ == '__main__':
+    from ConsoleIO import WebIO, ConsoleIO
+else:
+    from .ConsoleIO import WebIO, ConsoleIO
+
+
+class Student:
+    def __init__(self, name='', surname='', age='', io=ConsoleIO()):
+        self.__name = name
+        self.__surname = surname
+        self.__age = age
+        self.__io = io
+
+    '''@property
+    def out(self):
+        return self.__output'''
+
+    '''@out.setter
+    def out(self, output) -> None:
+        self.__output = output'''
+
+    def output(self):
+        self.__io.do_output(self)
+
+    def input(self):
+        self.__io.do_input(self)
+
+    def name_and_age(self):
+        return f'Имя: { self.__name }, Возраст: { self.__age }'
+
+    def __str__(self):
+        return "Имя: {}, Фамилия: {}, Возраст: {}".format(
+            self.__name, self.__surname, self.__age
+        )
